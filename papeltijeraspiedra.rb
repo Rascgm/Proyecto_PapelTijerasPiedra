e0f143c55f52df5004f065e1a8e3fa59c4298fb8
Rascgm <csar.ravelo@gmail.com>
2012-09-25 22:43:07 +0100

    Insertada regulacion de derrota del jugador

--- papeltijeraspiedra.rb
+++ papeltijeraspiedra.rb
@@ -9,3 +9,30 @@ puts comp
 jugador = ARGV.shift || jugadas.sample
 
 puts jugador
+
+if comp == jugador
+	
+	puts "> Empate; ambos sacaron #{comp}"
+	
+	else
+		
+		g_com = true
+		g_play = true
+		
+		case
+			
+			when jugador == :piedra then g_play = false if comp == :papel 
+			when jugador == :papel then g_play = false if comp == :tijeras  
+			when jugador == :tijeras then g_play = false if comp == :piedra
+			
+		end
+		
+		text = 'Jugador'
+		
+		text = 'PC' if g_com == true 
+		
+		puts "> Gana el #{text}"
+		
+		
+		
+	end
